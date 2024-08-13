d6f6ec824331f33d41bbdcaf0d0f820caf1d58b0
AnishGzp <anishsinghgzp1024@gmail.com>
2024-08-13 20:28:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Star Rating Using React
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import StarRating from "./components/StarRating";
+
+function App() {
+  return (
+    <>
+      <StarRating noOfStars={10} />
+    </>
+  );
+}
+
+export default App;
